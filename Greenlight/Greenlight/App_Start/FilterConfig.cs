8025a6efe085bef79774c09681c5e970ca897500
Ryan Webb <dzanfox@gmail.com>
2016-03-08 15:18:02 -0600

    initial commit

---
+++ Greenlight/Greenlight/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Greenlight
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
